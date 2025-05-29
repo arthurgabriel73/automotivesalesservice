5ce22325ceaf65600abee65cfe1d466b0cb4cc03
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-05-29 01:04:17 -0300

    first commit

---
+++ src/main/kotlin/br/com/fiap/automotivesalesservice/AutomotivesalesserviceApplication.kt
@@ -0,0 +1,11 @@
+package br.com.fiap.automotivesalesservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AutomotivesalesserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<AutomotivesalesserviceApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/fiap/automotivesalesservice/AutomotivesalesserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.fiap.automotivesalesservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AutomotivesalesserviceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

88da2a51d4c9281eae851e3a6013082d2fbd601b
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-06-16 01:48:37 -0300

    refactor: SaveVehicleUseCase.kt

--- src/main/kotlin/br/com/fiap/automotivesalesservice/core/application/useCases/SaveVehicleUseCase.kt
+++ src/main/kotlin/br/com/fiap/automotivesalesservice/core/application/useCases/SaveVehicleUseCase.kt
@@ -4,25 +4,27 @@ import br.com.fiap.automotivesalesservice.core.application.ports.driven.VehicleR
 import br.com.fiap.automotivesalesservice.core.application.ports.driver.SaveVehicleDriverPort
 import br.com.fiap.automotivesalesservice.core.application.ports.driver.models.input.SaveVehicleInput
 import br.com.fiap.automotivesalesservice.core.domain.vehicle.Vehicle
+import java.util.*
 
 class SaveVehicleUseCase(val vehicleRepository: VehicleRepository) : SaveVehicleDriverPort {
     override fun execute(input: SaveVehicleInput) {
-        val vehicle = Vehicle(
-            vehicleId = input.vehicleId,
-                    make = input.make,
-                    model = input.model,
-                    version = input.version,
-                    yearFabrication = input.yearFabrication,
-                    yearModel = input.yearModel,
-                    kilometers = input.kilometers,
-                    color = input.color,
-                    plate = input.plate,
-                    price = input.price,
-                    priceCurrency = input.priceCurrency,
-                    status = input.status,
-                    createdAt = input.createdAt,
-                    updatedAt = input.updatedAt,
-        )
+        val vehicle =
+            Vehicle(
+                vehicleId = UUID.fromString(input.vehicleId),
+                make = input.make,
+                model = input.model,
+                version = input.version,
+                yearFabrication = input.yearFabrication,
+                yearModel = input.yearModel,
+                kilometers = input.kilometers,
+                color = input.color,
+                plate = input.plate,
+                price = input.price,
+                priceCurrency = input.priceCurrency,
+                status = input.status,
+                createdAt = input.createdAt,
+                updatedAt = input.updatedAt,
+            )
         vehicleRepository.save(vehicle)
     }
 }

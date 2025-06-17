803bdd7802ca6d1c9388d370d75bd906dd563138
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-06-17 00:45:16 -0300

    feat: DriverPort and IO Models

---
+++ src/main/kotlin/br/com/fiap/automotivesalesservice/core/application/ports/driver/models/input/CreateOrderInput.kt
@@ -0,0 +1,3 @@
+package br.com.fiap.automotivesalesservice.core.application.ports.driver.models.input
+
+data class CreateOrderInput(val vehicleId: String, val customerId: String)
---
+++ src/main/kotlin/br/com/fiap/automotivesalesservice/core/application/ports/driver/models/output/CreateOrderOutput.kt
@@ -0,0 +1,3 @@
+package br.com.fiap.automotivesalesservice.core.application.ports.driver.models.output
+
+data class CreateOrderOutput(val orderId: String)
